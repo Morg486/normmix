18404f0de1c5ee5a06ddbf8f3eb910fb8b1edefa
Morgan Cheurco <morgancheurco@Morgans-MBP-1.devices.kent.edu>
2025-02-13 16:07:43 -0500

    Created package skeleton

---
+++ normmix.R
@@ -0,0 +1,13 @@
+#' @param x The values where the density is evaluated
+#' @param mean1 
+#' 
+#' @return 
+#' @export
+#' 
+#' @examples
+
+dimx = 
+  code -> roxygen skeleton
+  mixprop
+  
+  #make sure project options -> build tools -> roxygen selected -> roxygen options all selected
